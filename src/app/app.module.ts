90075b5d9bdd90f9fc990650a711ad2692222b40
EstevenJaviier <estirastam.c@gmail.com>
2021-04-30 20:11:21 -0500

    20210430

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: AppComponent },
-      {
-        path: 'characters',
-        loadChildren: () =>
-          import('./modules/characters/characters.module').then(
-            (m) => m.CharactersModule
-          ),
-      },
-    ]),
-    StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-  ],
+  imports: [BrowserModule, RouterModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/modules/characters/state/character.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { createReducer, on } from '@ngrx/store';
-
-export const initialState = 0;
-
-const _counterReducer = createReducer(initialState);
-
-export function counterReducer(state, action) {
-  return _counterReducer(state, action);
-}
